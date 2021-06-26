6cef9e9b5a5ccbd94a82b60493653ed7de177491
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2021-06-26 18:02:22 -0300

    Fixed model definitions to follow typescript format

--- api/src/sequelize/app/models/user.ts
+++ api/src/sequelize/app/models/user.ts
@@ -1,7 +1,8 @@
 'use strict'
-import { DataTypes, Model } from 'sequelize'
+import { DataTypes, Model, Optional } from 'sequelize'
+import {sequelize} from '.'
 
-interface IUserModel {
+interface IUserAttributes {
 	id: number
 	login: string
 	password: string
@@ -9,23 +10,16 @@ interface IUserModel {
 	status: number
 }
 
-export default (sequelize, DataTypes) => {
-	class user extends Model<IUserModel> implements IUserModel {
-		/**
-		 * Helper method for defining associations.
-		 * This method is not a part of Sequelize lifecycle.
-		 * The `models/index` file will call this method automatically.
-		 */
-		id!: number
-		login!: string
-		password!: string
-		email!: string
-		status!: number
-		static associate(models) {
-			// define association here
-		}
-	}
-	user.init({
+interface IUserCreationAttributes extends Optional<IUserAttributes, 'id'> {}
+
+interface IUserInstance extends Model<IUserAttributes, IUserCreationAttributes>, IUserAttributes {
+	createdAt?: Date;
+	updatedAt?: Date;
+}
+
+const User = sequelize.define<IUserInstance>(
+	'User',
+	{
 		id: {
 			type: DataTypes.INTEGER,
 			autoIncrement: true,
@@ -50,9 +44,7 @@ export default (sequelize, DataTypes) => {
 			type: DataTypes.INTEGER,
 			allowNull: false
 		}
-	}, {
-		sequelize,
-		modelName: 'user',
-	})
-	return user
-}
+	}
+)
+
+export default User
