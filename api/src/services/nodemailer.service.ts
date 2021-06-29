561f8c824d593edc6223f54b21e4601925875b22
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2021-06-29 14:15:57 -0300

    Added nodemailer and created nodemailer service

---
+++ api/src/services/nodemailer.service.ts
@@ -0,0 +1,31 @@
+import { createTransport } from 'nodemailer'
+
+
+interface IMail {
+	from: string
+	to: string
+	subject?: string
+	text?: string
+	html?: string
+}
+
+const host		= process.env.MAILER_HOST
+const port		= process.env.MAILER_PORT
+const user		= process.env.MAILER_USER
+const pass		= process.env.MAILER_PASS
+const secure	= process.env.MAILER_SECURE
+
+const transporter = createTransport({
+	host: host && 'smtp.example.com',
+	port: port && 587,
+	auth: {
+		user: user && 'test@example.com',
+		pass: pass && '123456'
+	},
+	secure: secure && true
+})
+
+export {
+	transporter,
+	IMail
+}
