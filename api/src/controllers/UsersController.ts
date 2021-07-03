954e8a35ce1e9a44886557608689f4540aa25eba
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2021-07-03 18:59:00 -0300

    User confirmation email implementation postponed due technical issues

--- api/src/controllers/UsersController.ts
+++ api/src/controllers/UsersController.ts
@@ -39,51 +39,51 @@ const UserController = {
 
 			// Create jwt token for confirmation
 
-			const token = new JWToken({}).createToken({login, email})
+			// const token = new JWToken({}).createToken({login, email})
 
-			const link = `http://${request.hostname}/user/confirmation/${token}`
+			// const link = `http://${request.hostname}/user/confirmation/${token}`
 
-			// html content
+			// // html content
 
-			const html = `<!DOCTYPE html>
-			<html>
-				<head>
-					<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-					<link rel="preconnect" href="https://fonts.googleapis.com"/>
-					<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-					<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap" rel="stylesheet"/>
-				</head>
-				<body style="padding: 0; margin: 0; width: 100%;">
+			// const html = `<!DOCTYPE html>
+			// <html>
+			// 	<head>
+			// 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+			// 		<link rel="preconnect" href="https://fonts.googleapis.com"/>
+			// 		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+			// 		<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap" rel="stylesheet"/>
+			// 	</head>
+			// 	<body style="padding: 0; margin: 0; width: 100%;">
 					
-					<table width="100%" height="100%" style="background-color:#cdd4df; padding: 0; margin: 0" cellpading="0" cellspacing="0">
-						<tbody cellpading="0" cellspacing="0">
-							<tr cellpading="0" cellspacing="0">
-								<td align="center" cellpading="0" cellspacing="0">
-								<table style="margin: 0 auto; background:#f5f6fa; font-family: 'Roboto', Arial, Helvetica, sans-serif;" width="512">
-									<thead>
-										<tr>
-											<th><h1 style="font-weight: 300; padding: 10px; margin: 0; text-align: left;">Mercury App</h1></th>
-										</tr>
-									</thead>
-									<tbody>
-										<tr>
-											<td style="padding: 10px;">
+			// 		<table width="100%" height="100%" style="background-color:#cdd4df; padding: 0; margin: 0" cellpading="0" cellspacing="0">
+			// 			<tbody cellpading="0" cellspacing="0">
+			// 				<tr cellpading="0" cellspacing="0">
+			// 					<td align="center" cellpading="0" cellspacing="0">
+			// 					<table style="margin: 0 auto; background:#f5f6fa; font-family: 'Roboto', Arial, Helvetica, sans-serif;" width="512">
+			// 						<thead>
+			// 							<tr>
+			// 								<th><h1 style="font-weight: 300; padding: 10px; margin: 0; text-align: left;">Mercury App</h1></th>
+			// 							</tr>
+			// 						</thead>
+			// 						<tbody>
+			// 							<tr>
+			// 								<td style="padding: 10px;">
 												
-											</td>
-										</tr>
-										<tr>
-											<td style="padding: 10px;" align="center">
-												<a href="" style="text-decoration: none; border-radius: 5px; background: #0097e6; padding: 10px; color: white">Confirme sua inscrição</a>
-											</td>
-										</tr>
-									</tbody>
-								</table>
-								</td>
-							</tr>
-						</tbody>
-					</table>
-				</body>
-			</html>`
+			// 								</td>
+			// 							</tr>
+			// 							<tr>
+			// 								<td style="padding: 10px;" align="center">
+			// 									<a href="" style="text-decoration: none; border-radius: 5px; background: #0097e6; padding: 10px; color: white">Confirme sua inscrição</a>
+			// 								</td>
+			// 							</tr>
+			// 						</tbody>
+			// 					</table>
+			// 					</td>
+			// 				</tr>
+			// 			</tbody>
+			// 		</table>
+			// 	</body>
+			// </html>`
 
 			// const message: IMail = {
 			// 	to: `<${email}>`,
