3901882dbb2cdb8ae5f926fb226d5c8bcefb52df
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2021-07-05 13:26:18 -0300

    Changes controller's filename pattern again

--- api/src/router.ts
+++ api/src/router.ts
@@ -1,4 +1,4 @@
-import { UserController } from '@controllers/users.controller'
+import { UserController } from '@controllers/UserController'
 import { Router } from 'express'
 
 const router = Router()
