cffbff96414aedcb00902883f2a0174f01040c44
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2021-07-09 14:55:07 -0300

    Created component styles

---
+++ app/src/styles/components/ChatList/index.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+
+const stylesList = styled.ul`
+	display: flex;
+	flex-direction: column;
+	list-style: none;
+	margin: 0;
+	border: 0;
+
+	li {
+		display: flex;
+		flex-direction: row;
+	}
+`
