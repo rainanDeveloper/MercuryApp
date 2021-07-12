d2b9867393e354a7f87e0854fb76183853423c1f
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2021-07-12 13:59:00 -0300

    Created basic login interface

--- app/src/routes.jsx
+++ app/src/routes.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import { Home } from './pages/Home'
+import { Login } from './pages/Login'
 
 const Routes = ()=>{
     return (
         <BrowserRouter>
             <Switch>
                 <Route exact path="/" component={Home}/>
+                <Route exact path="/login" component={Login}/>
             </Switch>
         </BrowserRouter>
     )
---
+++ app/src/styles/pages/StyledLogin.js
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+
+const StyledLogin = styled.main`
+	display: flex;
+	flex-direction: column;
+	width: 100vw;
+	height: 100vh;
+	padding: 10px;
+	margin: 0;
+	align-items: center;
+
+	section.loginContainer {
+		display: flex;
+		flex-direction: column;
+		width: 100%;
+		max-width: 350px;
+		margin-top: 5%;
+	}
+
+	header.loginHeader {
+		display: flex;
+		flex-direction: column;
+		margin-bottom: 15px;
+		align-items: center;
+		justify-content: center;
+
+		span {
+			text-align: center;
+			font-size: 1.4em;
+			font-weight: 300;
+		}
+	}
+
+	form.login {
+		display: flex;
+		flex-direction: column;
+		padding: 15px;
+		background-color: #ecf0f1;
+		border-radius: 5px;
+		border: solid 1px #f4f8f9;
+		gap: 10px;
+		font-size: 1em;
+	}
+`
+
+export { StyledLogin }
