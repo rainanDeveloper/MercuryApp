efcd7fdd7c13b8d41e3c73fdf267334b1c1514fd
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2021-07-13 23:45:57 -0300

    Finished login page

---
+++ app/src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { StyledHome } from '../styles/pages/StyledHome'
+
+
+function Home() {
+	
+		
+	return (
+		<StyledHome>
+			<header>
+				<h1>Mercury App</h1>
+				<div className="initialPageButtons">
+					<Link to="./login">Login</Link>
+					<Link to="./signup">Sign Up</Link>
+				</div>
+			</header>
+		</StyledHome>
+	)
+}
+
+export {Home}
--- app/src/routes.jsx
+++ app/src/routes.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import { Dashboard } from './pages/Dashboard'
+import { Home } from './pages/Home'
 import { Login } from './pages/Login'
 
 const Routes = ()=>{
+
+	const authtoken = localStorage.getItem('authtoken')
+
+	var initialPage = Home
+
+	if(authtoken){
+		initialPage = Dashboard
+	}
+
     return (
         <BrowserRouter>
             <Switch>
-                <Route exact path="/" component={Dashboard}/>
+                <Route exact path="/" component={initialPage}/>
                 <Route exact path="/login" component={Login}/>
             </Switch>
         </BrowserRouter>
