f69257001d166275dbe687378ea427712ada5a77
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2021-07-19 14:32:10 -0300

    Added chat listing using auth info

--- app/src/pages/Dashboard.jsx
+++ app/src/pages/Dashboard.jsx
@@ -1,14 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {Chatlist} from '../components/ChatList/index.jsx'
 import { StyledDashboard } from '../styles/pages/StyledDashboard.js'
+import { useHistory } from 'react-router-dom';
+import { getAuthInfo } from '../services/AuthInfoService.js';
 
-function Dashboard() {
+function Dashboard({chatId=null}) {
+
+	const [chats, setChats]		= useState([])
+
+	const authtoken = localStorage.getItem('authtoken')
+
+	const history = useHistory()
 	
+	useEffect(async ()=>{
+		if(authtoken){
+			try{
+				const data = await getAuthInfo(authtoken)
+
+				if(data){
+					setChats(data.UserChats.map(c=>({
+						id: c.Chat.id,
+						title: c.Chat.name
+					})))
+				}
+			}
+			catch(error){
+				if(error?.response?.status == 401){
+					localStorage.clear()
+					window.location.pathname='/login'
+				}
+			}
+		}
+		else{
+			history.push('/login')
+		}
+	}, [])
 		
 	return (
 		<StyledDashboard>
 			<aside>
-				<Chatlist chatList={[{id: 1,  title: 'Duck', profilePicUrl:"https://duckduckgo.com/assets/logo_header.alt.v108.svg"}]}/>
+				<Chatlist chatList={chats}/>
 			</aside>
 		</StyledDashboard>
 	)
