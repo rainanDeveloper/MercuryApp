dd1cbb5af9b94fcb27f9dedc5059bcb6bfba4883
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2021-07-23 15:01:41 -0300

    Created initial chat list structure

--- app/src/styles/components/StyledChatMessageList/StyledMessageList/index.js
+++ app/src/styles/components/StyledChatMessageList/StyledMessageList/index.js
@@ -3,9 +3,25 @@ import styled from 'styled-components';
 
 const StyledMessageList = styled.section`
 	display: flex;
+	align-items: flex-end;
 	width: 100%;
 	gap: 15px;
 	flex-grow: 1;
+	padding-bottom: 10px;
+	overflow-y: scroll;
+	overflow-x: hidden;
+
+	ul {
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		list-style: none;
+		gap: 5px;
+
+		li {
+			width: 100%;
+		}
+	}
 
 `
 
