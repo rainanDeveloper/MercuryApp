95f664ccb9153c917f6255a41a41cd3ae5e7cd3b
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2021-07-27 23:10:09 -0300

    Created message controller, with store and index methods

---
+++ api/src/controllers/MessageController.ts
@@ -0,0 +1,44 @@
+import { Request, Response } from 'express'
+import { Message } from '@models/message'
+
+const MessageController = {
+	async index(request: Request, response: Response){
+		const {id: userId} = request['user']
+		const {ch} = request.query
+
+		const messages = await Message.findAll({
+			where: {
+				chatId: ch,
+				userId
+			}
+		})
+
+		return response.json(messages)
+	},
+	async store(request: Request, response: Response){
+		const {chatId, content, content_type} = request.body
+		const {id: userId} = request['user']
+
+		try{
+			const newMessage = await Message.create({
+				chatId,
+				userId,
+				content,
+				content_type
+			})
+	
+			return response.json(newMessage)
+		}
+		catch(error){
+			return response.status(500).json({
+				message: 'Error while trying to record the message',
+				error
+			})
+		}
+	}
+}
+
+
+export {
+	MessageController
+}
