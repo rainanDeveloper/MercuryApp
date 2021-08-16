595eda82e9e52464078edadb44b79b18b679c8f7
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2021-08-16 13:42:33 -0300

    Fixed bug on sign up page: the background color of signup form was setted to the right color (backgroundLight)

--- app/src/styles/pages/StyledSignUp.js
+++ app/src/styles/pages/StyledSignUp.js
@@ -36,7 +36,7 @@ const StyledSignUp  = styled.main`
 		display: flex;
 		flex-direction: column;
 		padding: 15px;
-		background-color: ${props=>props.theme.background};
+		background-color: ${props=>props.theme.backgroundLight};
 		border-radius: 5px;
 		border: solid 1px ${props=>props.theme.inputBorder};
 		gap: 10px;
