988bc660985d5c240bec37c459bc638d31cda899
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2021-12-08 12:14:23 -0300

    Changed table name for password recovery requests

--- api/src/sequelize/db/migrations/20211130151828-create-password-recovery-requests.js
+++ api/src/sequelize/db/migrations/20211130151828-create-password-recovery-requests.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('PasswordRecoveryRequests', {
+    await queryInterface.createTable('password_recovery_requests', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -28,6 +28,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('PasswordRecoveryRequests');
+    await queryInterface.dropTable('password_recovery_requests');
   }
 };
