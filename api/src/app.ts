0937480c8db976d4cf2cd3276d12eab4c728b050
Rainan Miranda de Jesus <rainan.jesus@pm.me>
2022-01-04 21:22:50 -0300

    Added swagger to routes

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,8 +1,20 @@
 
 import express from 'express';
 import { router } from './router';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as swaggerUi from 'swagger-ui-express';
+
 
 const app = express();
+
+const swaggerFile = path.join(process.cwd(), 'swagger.json');
+
+if (fs.existsSync(swaggerFile)) {
+  const swaggerData = fs.readFileSync(swaggerFile, 'utf8');
+  const swaggerDocument = JSON.parse(swaggerData);
+  app.use('/docs/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+}
 
 app.use(express.json());
 app.use('/', express.static('../app/build'));
@@ -10,6 +22,7 @@ app.use('/login', express.static('../app/build'));
 app.use('/signup', express.static('../app/build'));
 app.use('/resetPasswd', express.static('../app/build'));
 app.use('/chat/:id', express.static('../app/build'));
+
 
 app.use(router);
 
